c0b2c7772e4fc93f7cdda5bea9cb3a38dc692ddc
chanozignego <esteban.zignego@gmail.com>
2015-10-06 00:20:26 -0300

    add main to project

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        get("/", (req, res) -> {
+        	// Do something
+        	return "Welcome! This is a Spark Example!";
+        });
+        
+        get("/hello/:name", (req, res) -> {
+        	// Do something
+        	return "Welcome! " + req.params(":name");
+        });
     }
 }
